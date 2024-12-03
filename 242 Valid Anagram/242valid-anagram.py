75bf9a1cdf3c5fe40d56170c78098bd89a778cc2
Ricky Liu <rickyhl02@berkeley.edu>
2024-12-03 07:22:00 +0000

    Code challenge submission for question: 242 Valid Anagram

---
+++ 242 Valid Anagram/242valid-anagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = defaultdict(int)
+        if len(s) != len(t):
+            return False
+        for char in s:
+            count[char]  += 1
+        for char in t:
+            count[char] -= 1
+        for val in count.values():
+            if val!= 0:
+                return False
+        return True
+        
+        
